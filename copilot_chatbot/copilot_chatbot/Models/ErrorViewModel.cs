c3c10181e3293200f5bbda2633a96e45ad8e18b4
Yradouani <yasmine.radouani@outlook.fr>
2024-04-16 12:08:22 +0200

    first commit

---
+++ copilot_chatbot/copilot_chatbot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace copilot_chatbot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
